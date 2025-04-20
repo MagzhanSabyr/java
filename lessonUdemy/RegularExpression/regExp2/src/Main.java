def2c13e833eed1eced5891b311f9712f45e2903
magzhansabyr <magzhan.sabyr@gmail.com>
2025-04-20 12:16:04 +0500

    java Udemy regExp(Регулярные  выражения)1&2, Pattern & Matcher class 20.04.2025

--- lessonUdemy/RegularExpression/regExp2/src/Main.java
+++ lessonUdemy/RegularExpression/regExp2/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Arrays;
+
 public class Main{
     public static void main(String[] args) {
-    
-    
+        String a = "Hello123123there123213hey123213afdlj";
+        String[] words = a.split("\\d+");
+        System.out.println(Arrays.toString(words));
+        
+        String b = "Hello123123there123213hey123213a";
+        String modifiedString = b.replaceAll("\\d+", "-");
+        String modifiedString2 = b.replaceFirst("\\d+", "-");
+        System.out.println(modifiedString);
+        System.out.println(modifiedString2);
     
         
     }

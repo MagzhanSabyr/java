5348a1ea509f874444379410c5ba034e862c120c
magzhansabyr <magzhan.sabyr@gmail.com>
2025-05-06 14:46:34 +0500

    java SpringCourse  SpringMVC Валидация форм Annotation @Valid  06.05.2025

--- springJava/SpringMVC/CRUD_App1/src/main/java/maven/example/com/controllers/PeopleController.java
+++ springJava/SpringMVC/CRUD_App1/src/main/java/maven/example/com/controllers/PeopleController.java
@@ -1,10 +1,12 @@
 package maven.example.com.controllers;
 
+import jakarta.validation.Valid;
 import maven.example.com.dao.PersonDAO;
 import maven.example.com.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -42,7 +44,11 @@ public class PeopleController{
     }
     
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person ) {
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return "people/new";
+        }
         personDAO.save(person);
         
         return "redirect:/people";
@@ -55,7 +61,11 @@ public class PeopleController{
     }
     
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person,
+            BindingResult bindingResult, @PathVariable("id") int id){
+        if(bindingResult.hasErrors()){
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
